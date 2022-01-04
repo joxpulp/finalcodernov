69f15088fe17509b6ff1df470854a3bc63f3b2de
joxpulp <joxpulp@gmail.com>
2022-01-04 02:37:45 -0300

    Heroku

---
+++ dist/config/swagger.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.specs = void 0;
+var swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+var options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Coderhouse Final Project',
+            version: '0.0.1',
+            description: 'Ecommerce API Application',
+            contact: {
+                name: 'Josue',
+                email: 'joxpulp@gmail.com',
+            },
+        },
+        servers: [
+            {
+                url: 'https://coderecommerce.herokuapp.com/api',
+                description: 'Backend',
+            },
+        ],
+    },
+    apis: ['src/routes/*'],
+};
+exports.specs = swagger_jsdoc_1.default(options);
